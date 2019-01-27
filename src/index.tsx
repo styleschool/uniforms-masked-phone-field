8f056a35b8b674b1b68e7efbf7efbd3772f2d54b
ivansglazunov <ivansglazunov@gmail.com>
2019-01-27 19:53:37 +0300

    search field

--- src/index.tsx
+++ src/index.tsx
@@ -5,13 +5,17 @@ import { allCountries } from 'country-telephone-data';
 import { TextField as UniformsTextField, HiddenField } from 'uniforms-material';
 import BaseField from 'uniforms/BaseField';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
+import TextField from '@material-ui/core/TextField';
+import Paper from '@material-ui/core/Paper';
 
-var defaultMasks = [{
+var defaultMasks = [
+  {
     name: 'universal',
     iso2: 'universal',
     format: '*',
     dialCode: '',
-  }];
+  },
+];
 
 for (var m = 0; m < allCountries.length; m++) {
   if (allCountries[m].format) {
@@ -24,17 +28,12 @@ function createMask(lang: any, masks: any) {
     if (masks[i].iso2 === lang) {
       let code = (masks[i].dialCode + '').split('');
       let mask: any = masks[i].format.split('');
-      for (
-        let j = 0, count = 0;
-        j < mask.length && masks[i].format;
-        j++
-      ) {
+      for (let j = 0, count = 0; j < mask.length && masks[i].format; j++) {
         if (mask && mask[j] === '.') {
           if (code[count]) {
             mask[j] = code[count];
             count++;
-          }
-          else mask[j] = /\d/;
+          } else mask[j] = /\d/;
         }
       }
       if (mask && mask[0] === '*') mask = false;
@@ -46,13 +45,15 @@ function createMask(lang: any, masks: any) {
 class MaskedInput extends React.Component<any, any> {
   render() {
     const { inputRef, mask, ...props } = this.props;
-    return <TextMask { ...props }
+    return (
+      <TextMask
+        {...props}
         ref={inputRef}
         mask={mask}
         placeholderChar={'\u2000'}
         showMask={Boolean(mask)}
-        keepCharPositions
-      />;
+      />
+    );
   }
 }
 
@@ -61,12 +62,15 @@ export interface UniformsMaskedPhoneFieldStates {
   anchorEl?: HTMLElement | null;
   mask: maskArray | ((value: string) => maskArray);
   disabled: boolean;
+  search: string;
 }
 
 export interface UniformsMaskedPhoneFieldProps {
   showMenu: boolean;
   countries: any;
   regionFieldName: string;
+  recommendedCodes: number[];
+  search: boolean;
 }
 
 export default class UniformsMaskedPhoneField<
@@ -77,38 +81,48 @@ export default class UniformsMaskedPhoneField<
   state = {
     lang: this.context.uniforms.model[this.props.regionFieldName],
     anchorEl: null,
-    mask: createMask(this.context.uniforms.model[this.props.regionFieldName], this.props.countries),
+    mask: createMask(
+      this.context.uniforms.model[this.props.regionFieldName],
+      this.props.countries,
+    ),
     disabled: false,
+    search: '',
   };
   static defaultProps = {
     showMenu: true,
     countries: defaultMasks,
-    regionFieldName: 'region'
+    regionFieldName: 'region',
+    recommendedCodes: [],
   };
 
   handleClick = (event: any) =>
     this.setState({ anchorEl: event.currentTarget });
-  
-  handleClose = () => 
-    this.setState({ anchorEl: null });
+
+  handleClose = () => this.setState({ anchorEl: null });
 
   handleLang = (lang: any) =>
     this.setState({
       lang,
       anchorEl: null,
       mask: createMask(lang, this.props.countries),
-      disabled: true
+      disabled: true,
     });
   componentDidUpdate() {
     if (this.state.disabled) this.setState({ disabled: false });
-    }
+  }
   render() {
     const { anchorEl, lang } = this.state;
-    const { showMenu, regionFieldName, ...props } = this.props;
-    return <span>
-        {this.state.disabled ? null :
+    const {
+      showMenu,
+      regionFieldName,
+      recommendedCodes,
+      ...props
+    } = this.props;
+    return (
+      <span>
+        {this.state.disabled ? null : (
           <UniformsTextField
-            {... props}
+            {...props}
             inputProps={{ mask: this.state.mask }}
             InputProps={{
               inputComponent: MaskedInput,
@@ -127,6 +141,11 @@ export default class UniformsMaskedPhoneField<
                     anchorEl={anchorEl}
                     open={Boolean(anchorEl)}
                     onClose={this.handleClose}
+                    MenuListProps={{
+                      style: {
+                        paddingTop: 0,
+                      },
+                    }}
                     PaperProps={{
                       style: {
                         maxHeight: 216,
@@ -134,25 +153,52 @@ export default class UniformsMaskedPhoneField<
                       },
                     }}
                   >
-                    {this.props.countries.map((current: any) => (
-                      <MenuItem
-                        key={current.iso2}
-                        selected={current.iso2 === lang}
-                        onClick={() => this.handleLang(current.iso2)}
+                    {this.props.search !== false && (
+                      <Paper
+                        style={{
+                          position: 'sticky',
+                          background: 'white',
+                          top: 0,
+                          left: 0,
+                          width: '100%',
+                          zIndex: 1,
+                        }}
                       >
-                        {current.dialCode + ' ' + current.name}
-                      </MenuItem>
-                    ))}
+                        <TextField
+                          autoFocus={true}
+                          value={this.state.search}
+                          onChange={event =>
+                            this.setState({ search: event.target.value })
+                          }
+                        />
+                      </Paper>
+                    )}
+                    {this.props.countries.map((current: any) => {
+                      const text = current.dialCode + ' ' + current.name;
+                      if (
+                        this.state.search &&
+                        !~(' ' + text.toLowerCase()).indexOf(this.state.search)
+                      )
+                        return null;
+                      return (
+                        <MenuItem
+                          key={current.iso2}
+                          selected={current.iso2 === lang}
+                          onClick={() => this.handleLang(current.iso2)}
+                        >
+                          {text}
+                        </MenuItem>
+                      );
+                    })}
                   </Menu>
                 </InputAdornment>
-              ) : null ,
+              ) : null,
             }}
             label={false}
-          />}
-        <HiddenField
-          name={regionFieldName}
-          value={this.state.lang}
-        />
-      </span>;
+          />
+        )}
+        <HiddenField name={regionFieldName} value={this.state.lang} />
+      </span>
+    );
   }
 }
